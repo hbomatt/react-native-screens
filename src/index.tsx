a44cace4576aac183c48e3438f29e0f4f1707ae5
hbomatt <matthew.skibbs@hbo.com>
2022-03-20 19:49:49 -0700

    * Testing freeze with react-native-screens on Web.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Animated, View, ViewProps, ImageProps, Image } from 'react-native';
+import { version } from 'react-native/package.json';
+import { Freeze } from 'react-freeze';
 import {
   ScreenProps,
   ScreenContainerProps,
@@ -26,10 +28,73 @@ export function screensEnabled(): boolean {
   return ENABLE_SCREENS;
 }
 
+let ENABLE_FREEZE = false;
+
 // @ts-ignore function stub, freezing logic is located in index.native.tsx
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function enableFreeze(shouldEnableReactFreeze = true): void {
-  // noop
+  const minor = parseInt(version.split('.')[1]); // eg. takes 66 from '0.66.0'
+
+  // react-freeze requires react-native >=0.64, react-native from main is 0.0.0
+  if (!(minor === 0 || minor >= 64) && shouldEnableReactFreeze) {
+    console.warn(
+      'react-freeze library requires at least react-native 0.64. Please upgrade your react-native version in order to use this feature.'
+    );
+  }
+
+  ENABLE_FREEZE = shouldEnableReactFreeze;
+}
+
+export function freezeEnabled(): boolean {
+  return ENABLE_FREEZE;
+}
+
+interface FreezeWrapperProps {
+  freeze: boolean;
+  children: React.ReactNode;
+}
+
+// This component allows one more render before freezing the screen.
+// Allows activityState to reach the native side and useIsFocused to work correctly.
+function DelayedFreeze({ freeze, children }: FreezeWrapperProps) {
+  // flag used for determining whether freeze should be enabled
+  const [freezeState, setFreezeState] = React.useState(false);
+
+  if (freeze !== freezeState) {
+    // setImmediate is executed at the end of the JS execution block.
+    // Used here for changing the state right after the render.
+    setImmediate(() => {
+      setFreezeState(freeze);
+    });
+  }
+
+  return <Freeze freeze={freeze ? freezeState : false}>{children}</Freeze>;
+}
+
+function MaybeFreeze({ freeze, children }: FreezeWrapperProps) {
+  if (ENABLE_FREEZE) {
+    return <DelayedFreeze freeze={freeze}>{children}</DelayedFreeze>;
+  } else {
+    return <>{children}</>;
+  }
+}
+
+export class ScreenStack extends React.Component<ScreenStackProps> {
+  render(): JSX.Element {
+    if (ENABLE_FREEZE) {
+      const { children, ...rest } = this.props;
+      const size = React.Children.count(children);
+      // freezes all screens except the top one
+      const childrenWithFreeze = React.Children.map(
+        children,
+        (child, index) => (
+          <DelayedFreeze freeze={size - index > 1}>{child}</DelayedFreeze>
+        )
+      );
+      return <View {...rest}>{childrenWithFreeze}</View>;
+    }
+    return <View {...this.props} />;
+  }
 }
 
 export class NativeScreen extends React.Component<ScreenProps> {
@@ -47,12 +112,14 @@ export class NativeScreen extends React.Component<ScreenProps> {
         activityState = active !== 0 ? 2 : 0; // change taken from index.native.tsx
       }
       return (
-        <View
-          // @ts-expect-error: hidden exists on web, but not in React Native
-          hidden={activityState === 0}
-          style={[style, { display: activityState !== 0 ? 'flex' : 'none' }]}
-          {...rest}
-        />
+        <MaybeFreeze freeze={activityState === 0}>
+          <View
+            // @ts-expect-error: hidden exists on web, but not in React Native
+            hidden={activityState === 0}
+            style={[style, { display: activityState !== 0 ? 'flex' : 'none' }]}
+            {...rest}
+          />
+        </MaybeFreeze>
       );
     }
 
@@ -69,8 +136,6 @@ export const ScreenContainer: React.ComponentType<ScreenContainerProps> = View;
 export const NativeScreenContainer: React.ComponentType<ScreenContainerProps> = View;
 
 export const NativeScreenNavigationContainer: React.ComponentType<ScreenContainerProps> = View;
-
-export const ScreenStack: React.ComponentType<ScreenStackProps> = View;
 
 export const FullWindowOverlay = View;
 
